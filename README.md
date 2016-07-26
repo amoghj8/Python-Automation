292d09dea45ced7355c75324b541e16943d27b8e
amoghj8 <amoghj8@gmail.com>
2016-07-26 20:49:02 +0530

    Changes to barcelona_Schedule.py and README.md , also added real_Madrid_Schedule.py

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 
 Information about the scripts I have written. I have deveploped interest in web crawling and hence the scripts are about them. I would gladly accept inputs to improve the code and make it more efficient, or simplify the code. Do leave comments if you feel to do so.
 
-==>> barcelona_schedule.py  - This sends a notification regarding FCB's match details.
+==>> barcelona_schedule.py  - This sends a notification regarding FCB's match details and also mobile notification.
+
+==>> readl_Madrid_Schedule.py  - This sends a notification regarding RMCF's match details and also mobile notification.
 
 ==>> weather.py - Shows the temperature of particular city.Uses openweathermap API.
 
--- barcelona_schedule.py
+++ barcelona_schedule.py
@@ -7,7 +7,9 @@ gi.require_version('Notify','0.7')
 from gi.repository import Notify
 from gi.repository import GdkPixbuf
 import datetime
+import twilio
 
+#Request
 res = requests.get('https://www.fcbarcelona.com')
 bs_Obj = BeautifulSoup(res.text,'lxml')
 
@@ -22,8 +24,6 @@ mmp = bs_Obj.find('span',{'class':'match__main__phase'}).get_text().strip()
 match_time = bs_Obj.find('time',{'class':'match__main__date__data'}).get_text().split(' ')[2].strip()
 match_date = bs_Obj.find('time',{'class':'match__main__date__data'}).get_text().strip().split(' ')[0].split('/')
 mft = datetime.datetime(int(match_date[2]),int(match_date[1]),int(match_date[0]),int(match_time.split(':')[0]),int(match_time.split(':')[1]))
-
-#Adjusting according to IST
 mist = datetime.timedelta(hours=3,minutes=30)
 final_match_date_and_time  = str(mist+mft)
 
@@ -37,5 +37,16 @@ image = GdkPixbuf.Pixbuf.new_from_file('./Desktop/Python Stuff/Scraping/fc-barce
 nf = Notify.Notification.new(mmc+' : '+mmp,teamA+' vs '+teamB+'   '+final_match_date_and_time)
 nf.set_image_from_pixbuf(image)
 nf.show()
-nf.close()
 
+#Mobile Notification 
+from twilio.rest import TwilioRestClient
+time_1 = mist+mft
+time_2 = datetime.datetime.now()
+if((time_1-time_2).days==1):
+    account_sid =  "ACCSID"
+    auth_token = "TOKEN"
+    client = TwilioRestClient(account_sid, auth_token)     
+    message = client.messages.create(body="Matchday :"+'\n'+mmc+' : '+mmp+'\n'+teamA+' vs '+teamB+'\n'+'Kickoff on '+str(mist + mft),
+    to="+NO.1",    # Replace with your phone number
+    from_="+NO.2") # Replace with your Twilio number
+    print(message.sid)
