23ebf5361d30531a16169b22da0c98095a58f7bd
amoghj8 <amoghj8@gmail.com>
2016-07-30 19:48:34 +0530

    Modified real_Madrid_Schrdule.py

--- real_Madrid_Schedule.py
+++ real_Madrid_Schedule.py
@@ -8,54 +8,39 @@ import datetime
 import twilio
 from twilio.rest import TwilioRestClient
 
-res = requests.get('http://realmadrid.com/en')
+res = requests.get('http://realmadrid.com/en/football/schedule')
 bs_Obj = BeautifulSoup(res.text,'lxml')
 
 #Finding the timings of match
 
-match_date = bs_Obj.time.get_text().strip().split(sep=' - ')[0].split(sep='-')
-match_time = bs_Obj.time.get_text().strip().split(sep=' - ')[1]
+match_date = bs_Obj.time.get_text().split(sep='/')
+match_time = bs_Obj.find('time',{'class':'m_highlighted_next_game_time'}).get_text()[:-2]
 
 #Converting match timings to IST which is 3hrs 30 mins ahead of CEST
-mft = datetime.datetime(int(match_date[2]),int(match_date[1]),int(match_date[0]),int(match_time.split(':')[0]),int(match_time.split(':')[1]))
+mft = datetime.datetime(int(match_date[0]),int(match_date[1]),int(match_date[2]),int(match_time.split(':')[0]),int(match_time.split(':')[1]))
 mist = datetime.timedelta(hours=3,minutes=30)
 final_match_date_and_time  = str(mist + mft)
 
 #Teams and Competition info
-competition = bs_Obj.find('strong',{'itemprop':'superevent'}).get_text()
-teamA = bs_Obj.select('span strong')[0].get_text().upper()
-teamB = bs_Obj.select('span strong')[1].get_text().upper()
+competition = bs_Obj.p.findAll('span')[0].get_text()
+teamA = bs_Obj.select('.m_highlighted_next_game_team')[0].strong.get_text()
+teamB = bs_Obj.select('.m_highlighted_next_game_team')[1].strong.get_text()
 
 #Notification
-if(competition != 'TRAINING SESSION'):
-    Notify.init('Match')
-    image = GdkPixbuf.Pixbuf.new_from_file('./Desktop/Python Stuff/Scraping/30046showing.png')
-    nf = Notify.Notification.new(competition,teamA+' vs '+teamB+'   '+final_match_date_and_time)
-    nf.set_image_from_pixbuf(image)
-    nf.show()
-    nf.close()
-else:
-    Notify.init('Match')
-    image = GdkPixbuf.Pixbuf.new_from_file('./Desktop/Python Stuff/Scraping/30046showing.png')
-    nf = Notify.Notification.new(competition,final_match_date_and_time)
-    nf.set_image_from_pixbuf(image)
-    nf.show()
-    nf.close()
+Notify.init('Match')
+image = GdkPixbuf.Pixbuf.new_from_file('./Desktop/Python Stuff/Scraping/30046showing.png')
+nf = Notify.Notification.new(competition,teamA+' vs '+teamB+'   '+final_match_date_and_time)
+nf.set_image_from_pixbuf(image)
+nf.show()
     
 #Mobile Notification 
 time_1 = mist+mft
 time_2 = datetime.datetime.now()
 if((time_1-time_2).days<=1):
-    account_sid =  "Enter your sid"
-    auth_token = "Enter your token"
+    account_sid =  "ACf23e3efd69d2acab5a1d2ec0c74c1d8e"
+    auth_token = "5de990cc293d7fed34811a4aac49d556"
     client = TwilioRestClient(account_sid, auth_token)     
-    if(competition == 'TRAINING SESSION'):
-        message = client.messages.create(body=competition+'\n'+final_match_date_and_time,
-        to="Replace with your phone number"
-        from_="Replace with your Twilio number"
-        print(message.sid)
-    else: 
-        message = client.messages.create(body="Matchday :"+'\n'+competition+'\n'+teamA+' vs '+teamB+'\n'+'Kickoff on '+str(mist + mft),
-        to="Replace with your phone number"
-        from_="Replace with your Twilio number"
-        print(message.sid)
+    message = client.messages.create(body="Matchday :"+'\n'+competition+'\n'+teamA+' vs '+teamB+'\n'+'Kickoff on '+str(mist + mft),
+    to="Replace with your phone number",
+    from_="Replace with your Twilio number")
+    print(message.sid)
